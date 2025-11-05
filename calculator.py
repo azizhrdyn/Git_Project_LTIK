88f21e48539ef111f3cf797764aa9ca29d9d37d9
azizhrdyn <azizhardiyan@gmail.com>
2025-11-05 14:29:45 +0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,13 +1,15 @@
-while True:
-    try:
-        coef = int(input("Masukkan angka: "))
-    except ValueError:
-        continue
-    
-    print("---TABEL PERKALIAN---")
-    
-    for calculate in range (1,11):
-        if calculate < 11:
-            multiple = calculate*coef
-            print(f"{coef} x {calculate} = {multiple}")
-            calculate = calculate + 1
+def calculator():
+    while True:
+        try:
+            coef = int(input("Masukkan angka: "))
+        except ValueError:
+            continue
+        
+        print("---TABEL PERKALIAN---")
+        
+        for calculate in range (1,11):
+            if calculate < 11:
+                multiple = calculate*coef
+                print(f"{coef} x {calculate} = {multiple}")
+                calculate = calculate + 1
+calculator()
